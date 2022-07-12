ee21f12af958c7f8ea27f00f8cce89b51987a2b2
Koushik Maharaj <koushik.maharaj797@gmail.com>
2022-07-12 07:40:57 -0400

    formula

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,22 +4,6 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!!!!!!!!!1");
-        System.out.println("added line");
-        System.out.println("added line");
-        System.out.println("added line");
-        System.out.println("added line");
-        System.out.println("added line");
-        System.out.println("added line");
-        System.out.println("added line");
-        System.out.println("added line");
-        System.out.println("added line");
-        System.out.println("added line");
-        System.out.println("added line");
-        System.out.println("Hello World!!!!!!!!!!1");
-        System.out.println("Hello World!!!!!!!!!!1");
-        System.out.println("Hello World!!!!!!!!!!1");
-        System.out.println("Hello World!!!!!!!!!!1");
-        System.out.println("Hello World!!!!!!!!!!1");
+        System.out.println((10+1));
     }
 }
