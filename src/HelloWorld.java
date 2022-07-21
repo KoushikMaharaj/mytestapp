385fd0b5d915e9b3cefea1499762fa53ded6bc75
Koushik Maharaj <koushik.maharaj@hoonartek.com>
2022-07-21 12:08:57 +0530

    added line

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,5 +6,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println((10 + 1));
         System.out.println((10 + 2));
+        System.out.println((10 * 2));
+
     }
 }
